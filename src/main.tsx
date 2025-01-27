805b428ae4881bbd7dc7cec566a5edb4b9df6dc6
Pisarski Roman <pisarski.roman@gmail.com>
2025-01-27 09:46:46 +0100

    packagage

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
-import './index.css';
+import './index.css'
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
