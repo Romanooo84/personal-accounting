ad21d6eee798a05abd9d89032dd7fb3e2d555414
Pisarski Roman <pisarski.roman@gmail.com>
2025-01-27 09:31:43 +0100

    uodate

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { MantineProvider } from '@mantine/core';
 import './index.css';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MantineProvider>
       <App />
-    </MantineProvider>
   </StrictMode>
 );
